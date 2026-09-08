be228b6f604ef781eca7725250cbaa1f410c8ff9
martinaburotespinoza <noformal469@gmail.com>
2026-09-08 12:00:14 -0300

    test: cover extreme newsvendor quantiles

--- tests/test_extreme_quantile.py
+++ tests/test_extreme_quantile.py
@@ -4,7 +4,7 @@ from gurobean.model import _economic_unconstrained_q, expected_newsvendor_gradie
 
 
 def test_extreme_positive_critical_fractile_does_not_clip_to_z_minus_10():
-    lam = 0.05
+    lam = 80.0
     revenue = 1.0
     cost = 1.0 - 1e-16
     salvage = 0.0
@@ -15,10 +15,10 @@ def test_extreme_positive_critical_fractile_does_not_clip_to_z_minus_10():
     assert expected_newsvendor_gradient(q, lam, revenue, cost, salvage) <= 1e-12
 
 
-def test_normal_quantile_solution_is_stationary_for_tiny_demand():
-    lam = 0.0025
+def test_normal_quantile_solution_is_stationary_for_high_demand():
+    lam = 80.0
     revenue = 1.0
-    cost = 0.999999999999
+    cost = 1.0 - 1e-12
     salvage = 0.0
 
     q = _economic_unconstrained_q(lam, revenue, cost, salvage)
